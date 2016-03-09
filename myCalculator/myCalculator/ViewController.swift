4fc841ec19463679b796fbc31c2bc0884af2ebca
Michelle Lee <michelle.jing.lee@gmail.com>
2016-03-08 21:33:25 -0500

    added btn actions and labels

--- myCalculator/myCalculator/ViewController.swift
+++ myCalculator/myCalculator/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var ACbtnLabel: UIButton!
+    @IBOutlet weak var cBtnLabel: UIButton!
+    @IBOutlet weak var dotBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btns(sender: UIButton){
+        
     }
 
+    @IBAction func onPressMultipleBtn(sender: UIButton) {
+        
+    }
+   
+    @IBAction func onPressDivBtn(sender: UIButton) {
+        
+    }
+    
+    @IBAction func onPressMinusBtn(sender: UIButton) {
+        
+    }
+    
+    @IBAction func onPressAddBtn(sender: UIButton){
+        
+    
+    }
+    
+    @IBAction func onPressPercentBtn(sender: UIButton){
+        
+    }
+    
+    @IBAction func onPressACBtn(sender: UIButton){
+        
+    }
+    
+    @IBAction func onPressCBtn(sender: UIButton){
+        
+    }
+    
+    @IBAction func onPressDotBtn(sender: UIButton){
+        
+    }
 
 }
 
